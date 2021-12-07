4b6f8d64ee00fc8ca219f7655824f430abc35c81
eojin-dev <eojin-dev@naver.com>
2021-12-07 22:56:50 +0900

    자동배포 완료 시 디스코드 알림 추가

--- api/router.js
+++ api/router.js
@@ -1,20 +1,20 @@
-var express = require('express')
-var router = express.Router()
+var express = require("express");
+var router = express.Router();
 
-const availableLanguages = require('../availableLanguages')
-router.get('/availableLanguages', (req, res) => {
-    res.json({status: 200, availableLanguages})
-})
+const availableLanguages = require("../availableLanguages");
+router.get("/availableLanguages", (req, res) => {
+  res.json({ status: 200, availableLanguages });
+});
 
-router.get('/auth', (req, res) => {
-    if (!req.userId) {
-        res.json({status: 200, isAuthenticated: false});
-        return;
-    }
-    res.json({status: 200, isAuthenticated: true, id: req.userId});
-})
+router.get("/auth", (req, res) => {
+  if (!req.userId) {
+    res.json({ status: 200, isAuthenticated: false });
+    return;
+  }
+  res.json({ status: 200, isAuthenticated: true, id: req.userId });
+});
 
-router.use(require('./problems'))
-router.use(require('./solutions'))
+router.use(require("./problems"));
+router.use(require("./solutions"));
 
-module.exports = router
+module.exports = router;
